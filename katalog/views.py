9cacea9cfe01d762f23451cbfeb32675953a641d
unknown <matthewrizkyhartadi@gmail.com>
2022-09-13 23:16:35 +0700

    completed assignment 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
 # TODO: Create your views here.
+def show_katalog(request):
+    katalog_item = CatalogItem.objects.all()
+    context = {
+        'name': 'Matthew Rizky Hartadi',
+        'student_id': 2106720941,
+        'katalog_list': katalog_item 
+    }
+
+    return render(request, 'katalog.html', context)
