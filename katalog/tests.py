e59d2882260c81f695da8ff3eed36439520329af
matthewrizkyhartadi <112454126+Matthew-Rizky-Hartadi@users.noreply.github.com>
2022-09-15 00:09:21 +0700

    Update tests.py

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from django.urls import resolve
+from .views import show_katalog
 # Create your tests here.
+
+class KatalogTest(TestCase):
+    def test_katalog_url_exists(self):
+        response = Client().get('/katalog/')
+        self.assertEqual(response.status_code,200)
+    
+    def test_katalog_using_template(self):
+        response = Client().get('/katalog/')
+        self.assertTemplateUsed(response, 'katalog.html')
