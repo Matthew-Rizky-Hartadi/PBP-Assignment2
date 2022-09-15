2511196e945a9cd032e60b38e1216eae6ca30de6
unknown <matthewrizkyhartadi@gmail.com>
2022-09-15 09:16:27 +0700

    added testing

--- project_django/settings.py
+++ project_django/settings.py
@@ -144,3 +144,5 @@ for directory in [*STATICFILES_DIRS, STATIC_ROOT]:
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+WHITENOISE_MANIFEST_STRICT=False
