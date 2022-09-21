fa8fe695146e92e07373c8ab6c35bc3b638e6aaf
unknown <matthewrizkyhartadi@gmail.com>
2022-09-21 18:12:09 +0700

    assignment 3 progress

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
 from django.test import TestCase, Client
-=======
 from django.test import TestCase
->>>>>>> e59d2882260c81f695da8ff3eed36439520329af
 from django.urls import resolve
 from .views import show_katalog
 # Create your tests here.
@@ -15,7 +13,4 @@ class KatalogTest(TestCase):
     def test_katalog_using_template(self):
         response = Client().get('/katalog/')
         self.assertTemplateUsed(response, 'katalog.html')
-<<<<<<< HEAD
-    
-=======
->>>>>>> e59d2882260c81f695da8ff3eed36439520329af
+
---
+++ mywatchlist/models.py
@@ -0,0 +1,10 @@
+from logging.handlers import WatchedFileHandler
+from django.db import models
+
+# Create your models here.
+class MyWatchList(models.Model):
+    watched = models.CharField(max_length=50)
+    title = models.TextField()
+    rating = models.FloatField()
+    release_date = models.TextField()
+    review = models.TextField()
